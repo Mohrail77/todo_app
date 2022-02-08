6fddda4cd825fbd5718647f5e37b18afd629f254
mohra alfy <mohra.alfy77@gmail.com>
2022-02-08 22:19:15 +0200

    commit

--- lib/modules/archived_tasks/archived_tasks_screen.dart
+++ lib/modules/archived_tasks/archived_tasks_screen.dart
@@ -5,7 +5,6 @@ import 'package:todo_app/shared/cubit/cubit.dart';
 import 'package:todo_app/shared/cubit/states.dart';
 
 //new archive
-//new one
 
 class ArchivedTasksScreen extends StatelessWidget {
   const ArchivedTasksScreen({Key key}) : super(key: key);
